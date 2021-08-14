02c3b6b33e6454a8886af9441c49f5687701d372
adirahul <84671369+adirahul@users.noreply.github.com>
2021-08-14 20:24:17 +0530

    Update Coin_combn_II.cpp

--- Coin_combn_II.cpp
+++ Coin_combn_II.cpp
@@ -11,15 +11,17 @@ int main() {
 
   vector<vector<int>> dp(n+1, vector<int> (target+1,0));
   
-  // state      :     dp[i][x] -> no of ways of getting sum x using first i coins
-  // base case  :     dp[i][0] =0 , we have empty set with sum zero
-  // transion   :     from i-1 th coin we can go to ith coin to either pick the coin or leave it
-  for(int i=0;i<n;i++){
+  // State        :     dp[i][x] -> no of ways of getting sum x using first i coins
+  // Base case    :     dp[i][0] =0 , we have empty set with sum zero
+  // Transition   :     consider the ith coin,Either we didn't pick the coin, then there are dp[i-1][x] ways, otherwise,
+                         we picked the coin. Since we are allowed to pick it again, there are dp[i][x — value(ith coin)] (not dp[i-1][x —value(ith coin)]) ways
+ 
+ for(int i=0;i<n;i++){
     dp[i][0]=1;
   }
   for (int i = 1; i <= n; i++) {
     for (int j = 0; j <= target; j++) {
-      dp[i][j] = dp[i-1][j];                //if ith coin isn't added in sum
+      dp[i][j] = dp[i-1][j];                
 
       int left = j-x[i-1];                  // ith coin has value x[i-1]
       if (left >= 0) {
