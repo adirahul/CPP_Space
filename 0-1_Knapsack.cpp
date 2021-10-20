b5aa6acce3c13b3fc577afa555a4753a89a4c5bb
Aditya Raj <84671369+adirahul@users.noreply.github.com>
2021-10-20 14:45:13 +0530

    Update 0-1_Knapsack.cpp

--- 0-1_Knapsack.cpp
+++ 0-1_Knapsack.cpp
@@ -1,35 +1,29 @@
-/* 0-1 knapsack -> To buy max pages of bunch of books using some amount of money x*/
+/* 0-1 knapsack -> Function to return max value that can be put in knapsack of capacity W. */
 
-#include <bits/stdc++.h>
-using namespace std;
 template <class T, class C>
-void ckmax(T &a, C b) { a > b ?: a = b; }
+void mx(T &a, C b) { a > b ?: a = b; }
+using ll = long long;
 
+class Solution
+{
+    public:
+    int knapSack(int W, int wt[], int val[], int N) 
+    {
 
-int main() {
-  int n,money;
-  cin >> n >>money;
-
-  vector<int> price(n) , pages(n);
-
-  for(int &x:price)cin>>x;
-  for(int &x:pages)cin>>x;
-
-  vector<vector<int>> dp(n+1, vector<int> (money+1,0));
-  
-  /*state        :     dp[i][x]      ->   max pages we can get using sum atmost x only buying from first i books
-    base case    :     dp[0][x] =0   ->   we cant get any pages without any books
-    transition   :     we can goto ith item from (i-1)th to either pick the item or leave it                                
-    answer       :     dp[n][money]                    */
- 
-  for(int i=1; i<=n; i++){
-    for(int j=0; j<=money; j++){
-      dp[i][j] = dp[i-1][j];  //leave it
-
-      if(j-price[i-1]>=0){
-        ckmax(dp[i][j] , dp[i-1][j- price[i-1]] +pages[i-1]);   //pick it
-      }
+    vector<vector<int>> dp(N + 1, vector<int> (W + 1, 0));
+    // dp[i][j] = max val obtained using first i items, max capacity being j
+    
+    for(int i = 1; i <= N; i++)
+    {
+        for(int j = 0; j <= W; j++)
+        {
+            dp[i][j] = dp[i - 1][j];      //leave it
+          
+            if(j >= wt[i - 1])
+            mx(dp[i][j], dp[i - 1][j - wt[i - 1]] + val[i - 1]);    //pick it
+        }
     }
-  }
-  cout<< dp[n][money];
-}
+   
+    return dp[N][W];
+    }
+};
