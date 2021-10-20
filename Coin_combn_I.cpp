ecbca8aa7aa7f30c6cb7119a2131c06eb81d2dda
Aditya Raj <84671369+adirahul@users.noreply.github.com>
2021-10-20 18:00:12 +0530

    Update Coin_combn_I.cpp

--- Coin_combn_I.cpp
+++ Coin_combn_I.cpp
@@ -1,30 +1,48 @@
-#include <bits/stdc++.h>
-using namespace std;
 const int mod = 1e9+7;
 
 /*Number of distinct ways you can produce a money sum x using the available coins.*/
-int main(){
-  ios_base::sync_with_stdio(false);
-  cin.tie(NULL);
- 
-  int n, target;
-  cin >> n >> target;
-  int coin[n];
-  for (int i = 0; i < n; i++)
-    cin >> coins[i];
+class solution {
+  public:
+  long long solve(vector<int> coins, int n, int target) 
+  {
  
   vector<int> dp(target + 1, 0);  //no of ways to produce sum 'target'
  
-  dp[0]=1;    //we initialize dp[0] = 1, saying the empty set is the only way to make 0
+  dp[0] = 1;    //we initialize dp[0] = 1, saying the empty set is the only way to make 0
 
-  for(int i=1 ;i<=target ;++i){
-    for(int x: coins){                // can swap order of loops to get no of ordered ways to produce sum target
-      if(i-x>=0){
-      dp[i] += dp[i-x];
-      dp[i] =dp[i]%mod;
-      }
-    }
+  for(int i = 1; i <= target; ++i)
+      for(int x: coins)                // can swap order of loops to get no of ordered ways to produce sum target
+          if(i - x >= 0)
+            {
+              dp[i] += dp[i - x];
+              dp[i] = dp[i] % mod;
+            }
+     
+   
+   return dp[target];
   }
+};
 
-  cout<< dp[target];
-}
+
+//no of ordered ways to make sum = target  |  Also called Coin Change
+
+class Solution {
+  public:
+  using ll = long long;
+    long long coin_change(int S[], int N, int Target) {
+        vector<ll> dp(Target + 1, 0);
+        dp[0] = 1;
+        
+      for(int x = 0; x < N; x++)
+      {
+          for(int i = 0; i <= Target; i++)          // this code works coz we are processing every coin only once
+          {
+                  if(i - S[x] >= 0)
+                  dp[i] += dp[i - S[x]];
+                
+        
+          }
+      }
+    return dp[Target];
+  }
+};
